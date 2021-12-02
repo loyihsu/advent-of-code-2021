c5867605e5c1f2571d3fc86be0f3cea42d017966
Yu-Sung Loyi Hsu <loyihsu@icloud.com>
2021-12-02 16:43:06 +0800

    Add solutions for Day 2 puzzles.

--- advent-of-code-2021/main.swift
+++ advent-of-code-2021/main.swift
@@ -7,13 +7,44 @@
 
 import Foundation
 
-func solver_1(input: String) {
-
+enum CommandDirections: String {
+    case forward, up, down
 }
 
-func solver_2(input: String) {
-
+func solver_1(input: String) -> Int {
+    let parsedInput = getParsedStringsFromLinesAndSpaces(from: input)
+    var position = (horizontal: 0, vertical: 0)
+    for line in parsedInput {
+        if let command = line.first, let last = line.last, let value = Int(last) {
+            if command == CommandDirections.forward.rawValue {
+                position.horizontal += value
+            } else if command == CommandDirections.up.rawValue {
+                position.vertical -= value
+            } else if command == CommandDirections.down.rawValue {
+                position.vertical += value
+            }
+        }
+    }
+    return position.0 * position.1
 }
 
-//solver_1(input: getIoFile(for: .question1))
-//solver_2(input: getIoFile(for: .question2))
+func solver_2(input: String) -> Int {
+    let parsedInput = getParsedStringsFromLinesAndSpaces(from: input)
+    var position = (horizontal: 0, vertical: 0), aim = 0
+    for line in parsedInput {
+        if let command = line.first, let last = line.last, let value = Int(last) {
+            if command == CommandDirections.forward.rawValue {
+                position.horizontal += value
+                position.vertical += aim * value
+            } else if command == CommandDirections.up.rawValue {
+                aim -= value
+            } else if command == CommandDirections.down.rawValue {
+                aim += value
+            }
+        }
+    }
+    return position.0 * position.1
+}
+
+print(solver_1(input: getIoFile(for: .question1)))
+print(solver_2(input: getIoFile(for: .question1)))

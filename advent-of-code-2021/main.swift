3a3a1ac58db8ef2195b3ce41d57a4dd4bd9de535
Yu-Sung Loyi Hsu <loyihsu@icloud.com>
2021-12-06 17:09:49 +0800

    Add solution for day 6.

--- advent-of-code-2021/main.swift
+++ advent-of-code-2021/main.swift
@@ -7,13 +7,19 @@
 
 import Foundation
 
-func solver_1(input: String) {
-
+func solver(input: String, days: Int) -> Int {
+    let list = input.trimmingCharacters(in: .newlines)
+        .components(separatedBy: ",")
+        .compactMap({ Int($0) })
+    var book = [Int](repeating: 0, count: 9)
+    for item in list {
+        book[item + 1] += 1
+    }
+    for idx in 0...days {
+        book[(idx + 7) % 9] += book[idx % 9]
+    }
+    return book.reduce(0, +)
 }
 
-func solver_2(input: String) {
-
-}
-
-//solver_1(input: getIoFile(for: .question1))
-//solver_2(input: getIoFile(for: .question2))
+print(solver(input: getIoFile(for: .question1), days: 80))
+print(solver(input: getIoFile(for: .question1), days: 256))

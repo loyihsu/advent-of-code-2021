0df02bf445ab65df253e891d128412183d92e20d
Yu-Sung Loyi Hsu <loyihsu@icloud.com>
2021-12-17 16:40:12 +0800

    Add solution for day 17.

--- advent-of-code-2021/main.swift
+++ advent-of-code-2021/main.swift
@@ -7,13 +7,31 @@
 
 import Foundation
 
-func solver_1(input: String) {
+func solver(input: String) -> (Int, Int) {
+    let area = input.trimmingCharacters(in: .newlines).components(separatedBy: "target area: ").joined().components(separatedBy: ", ")
+        .compactMap({ $0.components(separatedBy: "=").last })
+        .compactMap({ $0.components(separatedBy:"..") .compactMap({ Int($0) }) })
+    let targetArea = (x: area[0][0]...area[0][1], y: area[1][0]...area[1][1])
+    var max = Int.min, counter = 0
 
+    for x in 1...(targetArea.x.upperBound * 2) {
+        for y in (targetArea.y.lowerBound * 2)...(-targetArea.y.upperBound * 2) {
+            var highestY = Int.min, velocity = (x: x, y: y), point = (x: 0, y: 0)
+            while point.x <= targetArea.x.upperBound && point.y >= targetArea.y.lowerBound {
+                point = (point.x + velocity.x, point.y + velocity.y)
+                highestY = point.y > highestY ? point.y : highestY      // Update local max
+                // Update velocity
+                velocity.x = velocity.x - 1 <= 0 ? 0 : velocity.x - 1
+                velocity.y = velocity.y - 1
+                if targetArea.x.contains(point.x) && targetArea.y.contains(point.y) {
+                    max = highestY > max ? highestY : max   // Update global max
+                    counter += 1                            // Count distinct velocity that falls into the target area
+                    break
+                }
+            }
+        }
+    }
+    return (max, counter)
 }
 
-func solver_2(input: String) {
-
-}
-
-//solver_1(input: getIoFile(for: .question1))
-//solver_2(input: getIoFile(for: .question2))
+print(solver(input: getIoFile(for: .question1)))
